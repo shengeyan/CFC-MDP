359e77e152de0653346b0a1342a471e049e0824e
ShenGeYan <2323499160@qq.com>
2023-04-11 15:48:38 +0800

    左侧才菜单栏

---
+++ src/components/Menu.vue
@@ -0,0 +1,67 @@
+<template>
+    <br>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">收起</el-radio-button>
+    </el-radio-group>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>消息模板管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">新增模板</el-menu-item>
+          <el-menu-item index="1-2">模板列表</el-menu-item>
+        </el-menu-item-group>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <template #title>全链路追踪</template>
+      </el-menu-item>
+      <el-menu-item index="3" >
+        <el-icon><document /></el-icon>
+        <template #title>渠道物料管理</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <template #title>渠道账号管理</template>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <el-icon><setting /></el-icon>
+        <template #title>登录注册</template>
+      </el-menu-item>
+    </el-menu>
+  </template>
+  
+  <script scope setup>
+    import { ref } from 'vue'
+    import {
+      Document,
+      Menu as IconMenu,
+      Location,
+      Setting,
+    } from '@element-plus/icons-vue'
+    
+    const isCollapse = ref(true)
+    const handleOpen = (key, keyPath) => {
+      console.log(key, keyPath)
+    }
+    const handleClose = (key, keyPath) => {
+      console.log(key, keyPath)
+    }
+  </script>
+  
+  <style scoped>
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+      width: 200px;
+      min-height: 400px;
+    }
+
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
